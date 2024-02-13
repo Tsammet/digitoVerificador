5624ca2682db9d08e45adb29a3eaf5478dede9d5
Tsammet <carlos61658011@gmail.com>
2024-02-12 23:52:28 -0500

    terminando codigoverificador

---
+++ codigo.js
@@ -0,0 +1,61 @@
+let digitoVerificador = function(){
+
+    let numeroInvertido = 0;
+
+    let numero = document.getElementById("digito").value
+
+
+    while(numero > 0){
+
+        // sacamos el ultimo digito
+        let ultimoDigito = numero % 10;
+
+        // agregamos el ultimo digito multiplicandolo por 10 para luego meter el otro digito
+        numeroInvertido = numeroInvertido * 10 + ultimoDigito;
+
+        // eliminamos el último digito el número original para continuar con el siguiente
+        numero = Math.floor(numero / 10);
+
+    }
+    
+    return numeroInvertido
+}
+
+const numeroVerificador = function(){
+    
+    // Aquí convertí el numero invertido en un string para poder recorrer número por número
+    // también llamo la función aquí
+    numeroInvertido = digitoVerificador().toString();
+
+
+    let suma = 0;
+    let multiplicador = 2;
+    let limite = 7
+
+    // RECORRO núm x num
+    for (let i = 0; i<numeroInvertido.length; i++){
+
+        // AQUÍ CONVIERTO CADA NÚMERO QUE ES UN STRING A UN NUMERO
+        let digito = parseInt(numeroInvertido[i]);
+
+        suma += digito * multiplicador;
+
+        multiplicador++;
+
+        if (multiplicador > limite){
+            multiplicador = 2;
+        }
+    }
+
+
+    
+    modulo = suma % 11;
+    resta = 11 - modulo
+    
+    let num = document.getElementById("digito").value
+
+    let numverificador = num + "-" + resta
+
+    document.getElementById("digitoInvertido").innerHTML="el digito verificador es: " + numverificador
+
+}
